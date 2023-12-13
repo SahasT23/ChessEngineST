5e0b05905471f7d336dee21807f8fb1151cbc481
SahasT23 <108793094+SahasT23@users.noreply.github.com>
2023-12-13 11:29:07 +0000

    Update ChessMain.py
    first part, setting up the GameState and valid moves

--- ChessMainBody.py
+++ ChessMainBody.py
@@ -1,1 +1,25 @@
+import pygame as p
+import ChessEngine
 
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 # For animations later on
+IMAGES = {}
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wB', 'wN', 'wK', 'wQ', 'bp', 'bR', 'bB', 'bN', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("ChessPieces/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False
+
+    loadImages()
+    running = True
