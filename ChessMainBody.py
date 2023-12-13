ddf85657af1b68bb41140c539cd85a5f79ca95b7
SahasT23 <108793094+SahasT23@users.noreply.github.com>
2023-12-13 11:30:18 +0000

    Update ChessMain.py
    adding the ability to move the chess pieces and testing Pawn movement

--- ChessMainBody.py
+++ ChessMainBody.py
@@ -21,5 +21,68 @@ def main():
     validMoves = gs.getValidMoves()
     moveMade = False
 
-    loadImages()
+
+loadImages()
     running = True
+    sqSelected = () # No square is selected, keep track of the last click of the user (tuple: (row, col))
+    playerClicks = []  # Keeps track of player clicks (two tuples [(X1, Y1) --> (X2, Y2)])
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:  # Mouse handler
+                location = p.mouse.get_pos()  # X and Y location
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col):  # User has clicked the same square twice
+                    sqSelected = () # Deselects the square
+                    playerClicks = [] # Clears the player's clicks
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)  # Appends both the first and second clicks
+                if len(playerClicks) == 2: # After the second click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
+                    sqSelected = () # Resets the user's clicks
+                    playerClicks = []
+
+            elif e.type == p.KEYDOWN:  # Key handler
+                if e.key == p.K_z:
+                    gs.undoMove()
+                    moveMade = True
+
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen) # Draws the squares onto the board
+    # Add in piece highlighting and move suggestions
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):  # Top left square always has to be light
+    colors = [p.Color("white"), p.Color("dark green")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": # Not an empty square
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == "__main__":
+    main()
+
